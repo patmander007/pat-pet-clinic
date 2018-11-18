fafb8a6747f5611a955f25def8aa68abd8cde612
patmander007 <norman.santos@live.com>
2018-11-18 08:05:37 +0800

    Initial Project Add

---
+++ src/main/java/com/patmander/patpetclinic/PatPetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.patmander.patpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatPetClinicApplication.class, args);
+	}
+}
