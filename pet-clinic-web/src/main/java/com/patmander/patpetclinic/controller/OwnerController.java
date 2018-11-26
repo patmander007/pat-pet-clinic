95d7e117fd9e0948d7e5a9eba343eac337ef24a3
patmander007 <norman.santos@live.com>
2018-11-27 05:48:47 +0800

    closes #17

--- pet-clinic-web/src/main/java/com/patmander/patpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/patmander/patpetclinic/controller/OwnerController.java
@@ -1,16 +1,27 @@
 package com.patmander.patpetclinic.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.patmander.patpetclinic.services.OwnerService;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
   
+  private OwnerService ownerService;
+  
+  @Autowired
+  public OwnerController(OwnerService ownerService) {
+    this.ownerService = ownerService;
+  }
+  
   @RequestMapping({"","/","/index*"})
-  public String listVets(Model model) {
+  public String listOwners(Model model) {
     model.addAttribute("title", "Pet Clinic - Owners");
+    model.addAttribute("owners", ownerService.findAll());
+    
     return "owners/index";
   }
   
